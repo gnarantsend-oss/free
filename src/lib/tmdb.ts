57b9e7cc62179ad4011739db0442209d141ead7d
gnarantsend-oss <gnarantsend@gmail.com>
2026-04-10 21:10:33 +0800

    Update tmdb.ts

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -2,7 +2,7 @@
 import { type Movie, type TMDBResponse, type TVShow } from './types';
 
 const TMDB_API_URL = 'https://api.themoviedb.org/3';
-const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
 const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
 
 if (!TMDB_API_KEY) {
